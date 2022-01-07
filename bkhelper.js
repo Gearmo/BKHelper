82958cc1e2b665575a38bc8add81094a939d1a6a
Gearmo <93457954+Gearmo@users.noreply.github.com>
2022-01-07 13:15:32 -0800

    js file

---
+++ bkhelper.js
@@ -0,0 +1,13 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+/* if ($('input.checkbox_check').is(':unchecked')) {
+    // change opacity to 0 (css)
+} */
+
+$("input.checkbox_check").click(function(){
+    $("h1, h2, p").addClass("blue");
+    $("div").addClass("important");
+  });
