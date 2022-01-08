280ee41b34611b9e19e72470f43c6186947ca634
Gearmo <93457954+Gearmo@users.noreply.github.com>
2022-01-08 15:58:04 -0800

    Update bkhelper.js

--- bkhelper.js
+++ bkhelper.js
@@ -3,11 +3,40 @@ script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-/* if ($('input.checkbox_check').is(':unchecked')) {
-    // change opacity to 0 (css)
-} */
+/*
+$(function() {
+    $("checkbox").prop('checked', false) {
+        $(".mapobjects").css({"opacity": "0"});
+    });
+});
 
-$("input.checkbox_check").click(function(){
-    $("h1, h2, p").addClass("blue");
-    $("div").addClass("important");
-  });
+OR
+
+$(function() {
+    $("#reg").click(function () {
+        $(".mapobjects").toggle(1000);
+    });
+});
+
+if ($("#checkboxId").prop('checked', true)
+
+$("#checkboxId").prop('checked', false)
+
+OR
+
+if ($("checkbox").prop('checked')) {
+    $(".mapobjects").css("opacity", "1");
+} else {
+    $(".mapobjects").css("opacity", "0");
+}
+
+                                                                            HELP
+*/
+
+$('checkbox').on('item',function(e) {
+    if ($(this).prop('checked')) {
+        $('.mapobjects').css("opacity", "1");
+    } else {
+        $('.mapobjects').css("opacity", "0");
+    };
+});
